f7fe08e9df7cee61ff549ae546bebab9137248f7
Mikael Jantunen <mikael.jantunen@gmail.com>
2022-04-02 19:49:18 +0300

    fixed bug when getting data from database

--- db/schema.py
+++ db/schema.py
@@ -9,21 +9,20 @@ meta = MetaData()
 Base = declarative_base()
 
 class Tweet(Base):
-    __tablename__ = 'tweet'
+    __tablename__ = 'tweets_table'
     id = Column(Integer, primary_key=True)
     text = Column(String)
     created_at = Column(DateTime)
     lang = Column(String)
     source = Column(String)
-    retweet_count = Column(Integer)
-    like_count = Column(Integer)
-    quote_count = Column(Integer)
+    retweets = Column(Integer)
+    likes = Column(Integer)
+    quotes = Column(Integer)
 
     def __repr__(self):
-        return "<Tweet(text='%s', created_at='%s', lang='%s', source='%s', retweet_count='%s', like_count='%s', quote_count='%s')>" % (self.text, self.created_at, self.lang, self.source, self.retweet_count, self.like_count, self.quote_count)
+        return "<Tweet(text='%s', created_at='%s', lang='%s', source='%s', retweets='%s', likes='%s', quotes='%s')>" % (self.text, self.created_at, self.lang, self.source, self.retweets, self.likes, self.quotes)
 
     def add2db(data_df):
-
 
         data_df.to_sql(
             name ='tweets_table',
@@ -32,3 +31,5 @@ class Tweet(Base):
             index = True,
             chunksize = 100,
             dtype = {'text' : Text,'created_at' : DateTime, 'lang' : String(5), 'source' : Text, 'retweet_count' : Integer,'like_count' : Integer,'quote_count' : Integer})
+
+    
