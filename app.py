56a2c70578333f3a34994fb8e254cdfc955e0402
Mikael Jantunen <mikael.jantunen@gmail.com>
2022-04-03 20:52:22 +0300

    Added simple API - convert data from database to json for usage - added api functions - get all tweets   * /api/all - get top 10 by likes   * /api/top10likes - get tweets ordered by creation time   * /api/orderbytime - call the twitter bot from url to get new data in database   * /api/insert

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask
+from db.data import get_tweets, get_top10_tweets_by_likes, get_tweets_by_time
+from api import generate_json
+from twitter_bot.bot import get_tweets_from_twitter, tweets_to_df
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+@app.route('/api/all')
+def all_tweets():
+    return generate_json(get_tweets())
+    
+@app.route('/api/top10likes')
+def top10_likes():
+    return generate_json(get_top10_tweets_by_likes())
+
+@app.route('/api/orderbytime')
+def orderbytime():
+    return generate_json(get_tweets_by_time())
+
+@app.route('/api/insert')
+def more_tweets():
+    tweets = get_tweets_from_twitter()
+    tweets_to_df(tweets)
+    return "new data get"
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
